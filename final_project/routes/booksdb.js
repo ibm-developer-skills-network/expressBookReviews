3def51f6ef0882e98b3809ce72fadc8642f0ecb4
Sklup55 <ksundararajan@skillup.tech>
2022-09-12 13:07:09 +0000

    3rd update - 120922

--- final_project/routes/booksdb.js
+++ final_project/routes/booksdb.js
@@ -1,68 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-let books = [
-    {
-      "isbn": 1,
-      "author": "Chinua Achebe",
-      "title": "Things Fall Apart",
-      "review": "Good"
-    },
-    {
-      "isbn": 2,
-      "author": "Hans Christian Andersen",
-      "title": "Fairy tales",
-      "review": "Good"
-    },
-    {
-      "isbn": 3,
-      "author": "Dante Alighieri",
-      "title": "The Divine Comedy",
-      "review": "Good"
-    },
-    {
-      "isbn": 4,
-      "author": "Unknown",
-      "title": "The Epic Of Gilgamesh",
-      "review": "Good"
-    },
-    {
-      "isbn": 5,
-      "author": "Unknown",
-      "title": "The Book Of Job",
-      "review": "Good"
-    },
-    {
-      "isbn": 6,
-      "author": "Unknown",
-      "title": "One Thousand and One Nights",
-      "review": "Good"
-    },
-    {
-      "isbn": 7,
-      "author": "Unknown",
-      "title": "Nj\u00e1l's Saga",
-      "review": "Good"
-    },
-    {
-      "isbn": 8,
-      "author": "Jane Austen",
-      "title": "Pride and Prejudice",
-      "review": "Good"
-    },
-    {
-      "isbn": 9,
-      "author": "Honor\u00e9 de Balzac",
-      "title": "Le P\u00e8re Goriot",
-      "review": "Good"
-    },
-    {
-      "isbn": 10,
-      "author": "Samuel Beckett",
-      "title": "Molloy, Malone Dies, The Unnamable, the trilogy",
-      "review": "Good"
-    }
-]
+let books = {
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "copies":10, "reviews": {} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "copies":10, "reviews": {} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "copies":10, "review": {} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh","copies":10, "review": {} },
+      5: {"author": "Unknown","title": "The Book Of Job","copies":10, "review": {} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights","copies":10, "review": {} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga","copies":10, "review": {} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice","copies":10, "review": {} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot","copies":10, "review": {} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy","copies":10, "review": {} }
+}
 
 module.exports=books;
 
--- final_project/routes/general.js
+++ final_project/routes/general.js
@@ -4,41 +4,46 @@ const books = require('./booksdb.js');
 
 
 // Get the book list available in the shop
-router.get("/",(req,res)=>{
-    res.send(books);
-});
+router.get('/',function (req, res) {
+    res.send(JSON.stringify({books}, null, 4));
+ });
+ 
 
-
-
-// Get the based on isbn
-router.get("/:isbn",(req,res)=>{
+// Get book details based on ISBN
+router.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    let filtered_books = books.filter((book) => book.isbn === isbn);
-    res.send(filtered_books);
-}); 
+    res.send(books[isbn])
+    });
+    
 
+// Get book details based on author
+router.get('/author/:author',function (req, res) {
+    a = res.send(JSON.stringify({books}, null, 4));
+    JSON.parse(a);
+    // const author = req.params.author;
+    // a = Object.keys(books).filter(books[key][author] == req.params.author)
+    // console.log(a);
+});    
 
-// Get all books by author
-router.get("/:author",(req,res)=>{
-    const email = req.params.author;
-    let filtered_books = books.filter((book) => book.author === author);
-    res.send(filtered_books);
-}); 
 
 
 // Get all books based on title
-router.get("/:title",(req,res)=>{
-    const email = req.params.title;
-    let filtered_books = books.filter((book) => book.title === title);
-    res.send(filtered_books);
-}); 
+
+router.get('/title/:title',function (req, res) {
+    const title = req.params.title;
+    var books_based_on_title = JSON.parse(books).filter(function (book) {
+        return book.title === req.params.title;
+    });
+
+});
+
 
 //  Get book review
-router.get("/:review",(req,res)=>{
-    const email = req.params.review;
-    let filtered_books = books.filter((book) => book.review === review);
-    res.send(filtered_books);
-}); 
+
+router.get('/review/:isbn',function (req, res) {
+    const isbn = req.params.isbn;
+    res.send(books[isbn]["reviews"])
+});
 
 
 // Sign in as Customer
@@ -46,7 +51,6 @@ router.post("/",(req,res)=>{
     users.push({"firstName":req.query.firstName,"lastName":req.query.lastName,"ph_no":req.query.ph_no,"email":req.query.email});
     res.send("The user /n" + (req.query.firstName) + (req.query.lastName) + "has been added!")
 }); 
-
 
 
 module.exports=router;
