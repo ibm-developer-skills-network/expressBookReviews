36532fc11f7bc49ea3d389a8596037eef213b856
lavskillup <lavanyas@skillup.tech>
2022-09-13 12:10:54 +0530

    Added session code

--- practice_project/index.js
+++ practice_project/index.js
@@ -1,13 +1,51 @@
 const express = require('express');
-
-const routes = require('./router/practiceuser.js')
-
+const jwt = require('jsonwebtoken');
+const session = require('express-session')
+const routes = require('./router/users.js')
 
 const app = express();
 
-const PORT =5000;
+app.use(express.json());
+
+app.use(session({secret:"fingerpint",resave: true, saveUninitialized: true}))
 
 app.use(express.json());
+
+app.use("/user", function auth(req,res,next){
+   let token = req.session.authorization;
+   if(token) {
+       token = token['accessToken'];
+       jwt.verify(token, "access",(err,user)=>{
+           if(!err){
+               req.user = user;
+               next();
+           }
+           else{
+               return res.status(403).json({message: "User not authenticated"})
+           }
+        });
+    } else {
+        return res.status(403).json({message: "User not logged in"})
+    }
+});
+
+app.post("/login", (req,res) => {
+  const user = req.body.user;
+  if (!user) {
+      return res.status(404).json({message: "Error logging in"});
+  }
+  let accessToken = jwt.sign({
+      data: user
+    }, 'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+      accessToken,
+  }
+  return res.status(200).send("User successfully logged in");
+});
+
+const PORT =5000;
+
 app.use("/user", routes);
 
 app.listen(PORT,()=>console.log("Server is running"));
