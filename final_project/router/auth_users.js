66f14d1a54a9ec5c47605ceb71f345a0cbb28330
Ravina <parveravina@gmail.com>
2023-01-04 21:00:01 +0530

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,58 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+      });
+      if (userswithsamename.length > 0) {
+        return true;
+      } else {
+        return false;
+      }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user) => {
+        return user.username === username && user.password === password;
+      });
+      if (validusers.length > 0) {
+        return true;
+      } else {
+        return false;
+      }
 }
 
+app.use(express.json());
+
+app.use(session({ secret: "fingerpint" }));
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.query.username;
+    const password = req.query.password;
+  
+    if (!username || !password) {
+      return res.status(404).json({ message: "Error logging in" });
+    }
+  
+    if (authenticatedUser(username, password)) {
+      let accessToken = jwt.sign(
+        {
+          data: password,
+        },
+        "access",
+        { expiresIn: 60 * 60 }
+      );
+  
+      req.session.authorization = {
+        accessToken,
+        username,
+      };
+      return res.status(200).send("User successfully logged in");
+    } else {
+      return res
+        .status(208)
+        .json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
