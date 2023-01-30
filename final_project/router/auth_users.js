31a232c3b9f4d4022968742b124175fa17180b61
TheCarpenter73 <thecarpenter73@yahoo.com>
2023-01-30 16:07:53 -0500

    updated files

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,29 +1,38 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
+const app = express();
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+  //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const doesExist = (username) => {
+  let userswithsamename = users.filter((user) => {
+    return user.username === username
+  });
+  if (userswithsamename.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+const authenticatedUser = (username, password) => {
+  let validusers = users.filter((user) => {
+    return (user.username === username && user.password === password)
+  });
+  if (validusers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-// Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
