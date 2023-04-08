c7fdb7900d3371ae7db418d7e89db76a04d71e3a
anwar-tech <47064921+anwar-tech@users.noreply.github.com>
2023-04-08 23:55:50 +0300

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,7 +13,8 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify (books));
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
