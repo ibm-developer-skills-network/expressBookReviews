9a6ce42771d50f7321c218871c304c84fba0cdd3
Ashok Kumar <122712028+Ashok-Pavan@users.noreply.github.com>
2023-04-21 12:11:50 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,28 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next();
+            }
+            else {
+                return res.status(403).json({ message: "User not authenticated" })
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" })
+    }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
