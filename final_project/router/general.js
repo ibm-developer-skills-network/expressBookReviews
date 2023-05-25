28a9e3d6f300f4656fe767ef177a9d296093f4e5
Kylie <olivkylie@gmail.com>
2023-05-25 17:08:56 -0400

    completed task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,12 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
+  res.send(JSON.stringify(books, null, 4))
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
@@ -36,8 +34,8 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn]["reviews"])
 });
 
 module.exports.general = public_users;
