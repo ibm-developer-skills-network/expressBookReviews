cd81e9d6e7c33d1057d89803f825d00d2ca6403f
Kylie <olivkylie@gmail.com>
2023-05-25 18:19:26 -0400

    completed task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,19 +12,29 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books, null, 4))
+  res.send(JSON.stringify({books}, null, 4))
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
- });
+    const isbn = req.params.isbn;
+    res.send(books[isbn])
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["author"] === req.params.author) {
+        booksbyauthor.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+      }
+    });
+    res.send(JSON.stringify({booksbyauthor}, null, 4));
+  });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
