7e1f6e014daa9dc38ccdab42785844d1e915f71a
Kylie240 <113202977+Kylie240@users.noreply.github.com>
2023-05-26 13:57:06 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,25 +1,35 @@
 const express = require('express');
 let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
+let isInUse = require("./auth_users.js").isInUse;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-public_users.post("/register", (req,res) => {
-    users.push({"username":req.body.username,"password":req.body.password});
-    res.send("The user" + (' ')+ (req.body.username) + " Has been added!")
+
+public_users.post("/register/",(req,res)=>{
+    const username = req.query.username;
+    const password = req.query.password;
+
+    if (username && password) {
+      if (!isInUse(username)) {
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});
+      }
+    }
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.send(JSON.stringify({books}, null, 4))
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify({books}, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(books[isbn])
-});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
+ });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -54,6 +64,6 @@ public_users.get('/title/:title',function (req, res) {
 public_users.get('/review/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     res.send(books[isbn]["reviews"])
-});
+  });
 
 module.exports.general = public_users;
