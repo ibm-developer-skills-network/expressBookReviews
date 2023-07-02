ee79ff62ea12eec3b656736ab39e1b3fe7eaac6d
pxc <105520102+nocaprc@users.noreply.github.com>
2023-07-02 12:12:37 +0530

    hvvh

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,7 +12,7 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
+  res.send(JSON.stringify(books,null,4));
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
