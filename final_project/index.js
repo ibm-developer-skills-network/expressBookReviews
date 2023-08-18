d1bbef206e89e8d7e50e2220f426a80181a12575
greatxrider <daligdigjeph09@gmail.com>
2023-08-18 17:05:21 +0800

    updated index.js

--- final_project/index.js
+++ final_project/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const genl_routes = require('./router/general.js').general
 
 const app = express();
 
@@ -11,12 +11,26 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+//Write the authentication mechanism here
+    if(req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access", (err,user)=> {
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+        });
+    }
+    else {
+        return res.status(403).json({message: "User not logged in"})
+    }
 });
  
-const PORT =5000;
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
-
 app.listen(PORT,()=>console.log("Server is running"));
