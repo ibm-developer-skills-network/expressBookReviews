40da8432e6f811f114021df1cb291fea88a71ca0
Feiwen Cheng <feiwen_cheng@asus.com>
2023-09-14 13:55:49 +0800

    task5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -60,7 +60,12 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  res.send(book? book.reviews:'');
 });
 
 module.exports.general = public_users;
+
+
