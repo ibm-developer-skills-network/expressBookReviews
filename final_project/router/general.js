3a036f4b539d68d3787763a59316476a6e581f9f
Feiwen Cheng <feiwen_cheng@asus.com>
2023-09-14 11:00:34 +0800

    task1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,7 +13,8 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
