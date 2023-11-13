d4ef01eda08b36e07c8efa665bc40eec56a93cdb
mvishalsharma63 <65908715+mvishalsharma63@users.noreply.github.com>
2023-11-13 10:41:08 +0530

    index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,19 +1,97 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+
+let users = []
+
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const authenticatedUser = (username,password)=>{
+  let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(session({secret:"fingerpint"}))
 
+app.use(express.json());
+
+app.post("/login", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+  
+    if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+  
+      req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
+  });
+  
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    //Write the authenication mechanism here
+    if (req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access", (err,user)=>{
+            if(!err) {
+                req.user = user;
+                next();
+            }
+            else {
+                return res.status(403).json({message: "User not authenticated"})
+            }
+        });
+    } else {
+        return res.status(403).json({message: "user not logged in."})
+    }
 });
- 
+
+app.post("/register", (req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!doesExist(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
+});
+
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
