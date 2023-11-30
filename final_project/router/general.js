a410bb70473cb41fdbd8d31a65927160e96239b8
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-11-30 20:31:38 +0100

    make an endpoint to get reviews of a book by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -80,7 +80,23 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  // Find the book in the 'books' object based on the numeric index
+  const book = books[isbn];
+  if (book) {
+    // Check if the book has reviews
+    if (book.reviews && Object.keys(book.reviews).length > 0) {
+        // If reviews are found, send the reviews as JSON response
+        res.status(200).json(book.reviews);
+    } else {
+        // If no reviews found, send a message
+        res.status(404).json({ message: "No reviews found for the provided ISBN" });
+    }
+} else {
+    // If the book is not found, send a 404 response with an error message
+    res.status(404).json({ message: "Book not found with the provided ISBN" });
+}
 });
 
 module.exports.general = public_users;
