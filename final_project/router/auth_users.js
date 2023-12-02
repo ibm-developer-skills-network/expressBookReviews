ea6a87909f66044d934aa731c4f22578f2a76deb
SuperJunkman <150729960+SuperJunkman@users.noreply.github.com>
2023-12-02 15:21:44 -0600

    Update auth_users.js
    implemented isValid and authentucatedUser methods

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,12 +5,12 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{
+    return (users.filter(user => user.username === username) > 0)
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+    return (users.filter(user => {user.username === username && user.pasword === password}).length > 0)
 }
 
 //only registered users can login
