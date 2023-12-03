dd7b8e2b25af7ae43fb2bb0854b53baa8a52d6bb
SuperJunkman <150729960+SuperJunkman@users.noreply.github.com>
2023-12-02 20:39:25 -0600

    Update index.js
    miscellaneous fixes

--- final_project/index.js
+++ final_project/index.js
@@ -11,18 +11,18 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true})) 
 
 app.use("/customer/auth/*", function auth(req,res,next){
-    if(req.session.authorization) {
+    if (req.session.authorization) {
         token = req.session.authorization['accessToken'];
-        jwt.verify(toke, 'access', (err,user)=>{
-            if(!err){
+        jwt.verify(token, "access", (err,user)=>{
+            if(!err) {
                 req.user = user;
                 next();
             } else {
-                return res.status(403).json({message: "User not authenticated"})
+                return res.status(403).send("User not authenticated")
             }
         });
     } else {
-        return res.status(403).json({message: "User not logged in"})
+        return res.status(403).send("User not logged in")
     }
 });
  
