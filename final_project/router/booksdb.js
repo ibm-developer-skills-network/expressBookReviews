6f5e1d3cde355a1d29be9d8773486a99dfda846e
Emack2023 <147569908+Emack2023@users.noreply.github.com>
2023-12-18 23:47:13 -0500

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,5 +1,4 @@
-let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+  1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
       2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
       3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
       4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
@@ -9,6 +8,16 @@ let books = {
       8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
       9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
       10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {"admin": "Review  of Things Fall Apart"} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {"admin": "Review of Fairy tales"} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {"admin": "Review of The Divine Comedy"} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {"admin": "Review The Epic Of Gilgamesh"} },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {"admin": "Review of The Book Of Job"} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {"admin": "Review of One Thousand and One Nights"} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {"admin": "Review of Nj\u00e1l's Saga"} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {"admin": "Review of Pride and Prejudice"} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {"admin": "Review of Le P\u00e8re Goriot"} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {"admin": "Review of Molloy, Malone Dies, The Unnamable, the trilogy"} }
 }
 
 module.exports=books;
