1fafd177429b9c9c98b5ed433f8b9966b05a038d
Emack2023 <147569908+Emack2023@users.noreply.github.com>
2023-12-22 14:16:45 -0500

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,25 +5,74 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+    const isValid = (username)=>{
+        let userswithsamename = users.filter((user)=>{
+          return user.username === username
+        });
+        if(userswithsamename.length > 0){
+          return true;
+        } else {
+          return false;
+        }
+      }
+const authenticatedUser = (username,password)=>{
+    let validUsers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validUsers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  const app = express();
+  app.use(express.json());
+  app.use(session({secret:"fingerpint"}))
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+app.use("/auth", function auth(req,res,next){
+    if(req.session.authorization) { 
+        token = req.session.authorization['accessToken']; 
+        jwt.verify(token, "access",(err,user)=>{ 
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(300).json({message: "User not authenticated"})
+            }
+         });
+     } else {
+         return res.status(300).json({message: "User not logged in"})
+     }
+ });
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const username = req.params.username;
+    const password = req.params.password;
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+    if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+      req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
+  });
+  return res.status(300).json({message: "Yet to be implemented"});;
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    //Write your code here
+    return res.status(300).json({message: "Yet to be implemented"});
+  });
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
