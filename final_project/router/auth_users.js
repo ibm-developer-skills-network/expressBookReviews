8b599ec760eca8dcf98538a906d89e710a9d4be4
sumeetkuthari <sumeetkuthari@gmail.com>
2024-01-18 17:30:54 -0500

    Completed Task 7.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -19,12 +19,37 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+  let validusers = users.filter((user) => {
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if(!username || !password){
+    return res.status(404).json({message: "Error loggin in!"});
+  }
+  if(authenticatedUser(username, password)){
+    let accessToken = jwt.sign({
+        data: password
+    }, 'access', { expiresIn: 60 * 60});
+    req.session.authorization = {
+        accessToken, username
+    }
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid login. Check username and password"});
+  }
 });
 
 // Add a book review
