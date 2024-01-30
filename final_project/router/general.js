711d515628464445d783924cc01a280b152f47ba
Vrutika Patel <vrutikapatel1727@gmail.com>
2024-01-29 20:14:55 -0500

    added changes for login/register

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,9 +6,19 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+      if (!isValid(username)) {
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});
+      }
+    }
+    return res.status(404).json({message: "Unable to register user."});
+  });
+
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
