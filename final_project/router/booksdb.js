4b8777f402f2ecca6c9750c3fb29292725863553
Vrutika Patel <vrutikapatel1727@gmail.com>
2024-01-29 19:10:41 -0500

    added changes for getting reviews

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
       1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} ,"isbn": "abc"},
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {},"isbn": "8572" },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} ,"isbn": "8572"},
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {},"isbn": "8572" },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} ,"isbn": "8572"},
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {},"isbn": "8572"},
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} ,"isbn": "8572"},
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {},"isbn": "8572" },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {},"isbn": "8572" },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} ,"isbn": "8572"}
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": "Awesom book","isbn": "85721" },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} ,"isbn": "85722"},
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {},"isbn": "85723" },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} ,"isbn": "85724"},
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {},"isbn": "85725"},
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} ,"isbn": "85726"},
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {},"isbn": "85727" },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {},"isbn": "85728" },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} ,"isbn": "85729"}
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,14 +19,16 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     
-  const isbn = req.params.isbn;
-  const bookDetails = books[isbn];
-
-  if (bookDetails) {
-    res.send(bookDetails);
-  } else {
-    res.status(404).send('Book not found');
-  }
+    const isbn = req.params.isbn;
+    const booksByisbn = [];
+  
+    Object.keys(books).forEach((book) => {
+      if (books[book].isbn === isbn) {
+        booksByisbn.push(books[book]);
+      }
+    });
+  
+    res.send(booksByisbn);
 });
 
 // Get book details based on author
@@ -62,8 +64,18 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+
+    const booksByTitle = [];
+  
+    Object.keys(books).forEach((book) => {
+      if (books[book].isbn === isbn) {
+        booksByTitle.push(books[book]);
+      }
+    }); 
+    res.send(booksByTitle);
+
+    //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
