353c3c2ff5fae031cc74c66849603ca44b82f7e3
Desmond Talton <d.1phtgphy@gmail.com>
2024-01-31 10:55:01 -0500

    tasks1-5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,46 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let booksWithAuthor = {};
+    for (let isbn = 1; isbn <= Object.keys(books).length; isbn++) {
+        let currBook = books[isbn];
+        if (currBook.author === author) {
+            booksWithAuthor[isbn] = currBook;
+        }
+    }
+    res.send(JSON.stringify(booksWithAuthor, null, 4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let booksWithTitle = {};
+    for (let isbn = 1; isbn <= Object.keys(books).length; isbn++) {
+        let currBook = books[isbn];
+        if (currBook.title === title) {
+            booksWithTitle[isbn] = currBook;
+        }
+    }
+    res.send(JSON.stringify(booksWithTitle, null, 4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let book = books[isbn];
+    res.send(`Reviews for ${book.title} by ${book.author}: ${JSON.stringify(books[isbn].reviews, null, 4)}`);
 });
 
 module.exports.general = public_users;
