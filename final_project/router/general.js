cb5b7d2606ec494422ead5afe084d6e4c7e58f0b
José Ignacio Cruz Reyes <ignacio.cruz@theksquaregroup.com>
2024-02-20 18:54:52 -0500

    task1: get the list of books available in the shop

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,9 +11,8 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/", function (req, res) {
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
