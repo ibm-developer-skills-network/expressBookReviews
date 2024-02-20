1aea767c136e3d2d8da7ad4267d6031bc474e375
José Ignacio Cruz Reyes <ignacio.cruz@theksquaregroup.com>
2024-02-20 18:56:58 -0500

    task2: get the book details based on the ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -16,10 +16,14 @@ public_users.get("/", function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get("/isbn/:isbn", function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book) {
+    return res.status(200).send(JSON.stringify(book, null, 4));
+  }
+  return res.status(400).send(`Book with isbn ${isbn} not found.`);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
