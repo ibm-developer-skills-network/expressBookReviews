2d0e9861b405d8158db848098109c735a4c4c81d
karenLinceCelaya <89591373+karenLinceCelaya@users.noreply.github.com>
2024-02-21 12:08:18 -0600

    Update index.js
    mejoras

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,23 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+    //fingerprint_customer
+    if(req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access",(err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+         });
+     } else {
+         return res.status(403).json({message: "User not logged in"})
+     }
+
 });
  
 const PORT =5000;
@@ -19,4 +35,4 @@ const PORT =5000;
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log("Server is running karen granados"));
