9435ac2210e8c7086aab893e97e7695873a51f2f
José Ignacio Cruz Reyes <ignacio.cruz@theksquaregroup.com>
2024-02-21 10:13:24 -0500

    task7: add logic for logging in as a registered user

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -13,14 +13,44 @@ const isValid = (username) => {
   return userInfo.length > 0 ? false : true;
 };
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  //write code to check if username and password match the one we have in records.
+  const validUser = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+
+  return validUser.length > 0 ? true : false;
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "Error logging in, username &/ password were not provided.",
+    });
+  }
+
+  if (!authenticatedUser(username, password)) {
+    return res
+      .status(401)
+      .json({ message: "Invalid Login. Check username and password" });
+  }
+
+  const accessToken = jwt.sign(
+    {
+      data: password,
+    },
+    "access",
+    { expiresIn: 60 * 60 }
+  );
+  req.session.authorization = {
+    accessToken,
+    username,
+  };
+  return res.status(200).send("User successfully logged in");
 });
 
 // Add a book review
