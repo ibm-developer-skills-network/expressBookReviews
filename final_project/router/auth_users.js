272ede6c7869d4f86088a07b6b6eaf4ec4169e1c
karenLinceCelaya <89591373+karenLinceCelaya@users.noreply.github.com>
2024-02-21 12:07:11 -0600

    Update auth_users.js
    cargando mejoras

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,19 +10,152 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+      });
+      if(validusers.length > 0){
+        return true;
+      } else {
+        return false;
+      }
 }
 
-//only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+  
+    if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 });
+  
+      req.session.authorization = {
+        accessToken,username
+    } 
+    return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    
+    const islbC = req.params.isbn;
+    const libros = Object(books[islbC]);
+     if (Object.keys(libros).length>0) { 
+      let reviewC = req.body.reviews;
+       if(reviewC.length>0) 
+       {                
+                    let dataSet=[]
+                    let data
+                    let  bandera =true
+                    if(Array(libros)[0].reviews.length==undefined)
+                    {                    
+                       data = {
+                        "nombre": req.session.authorization.username,
+                        "review": reviewC
+                      }
+                      dataSet.push(data);
+                      //inserta porque es el primero
+                     }
+                    else
+                    { 
+                      bandera=true
+                      for (let i=0;i<Array(libros)[0].reviews.length;i++)
+                      {
+                         if(req.session.authorization.username==Array(libros)[0].reviews[i].nombre)
+                        {
+                         data = {
+                            "nombre": req.session.authorization.username,
+                            "review": reviewC
+                          }
+                          dataSet.push(data);
+                          bandera=false
+                        }
+                        else
+                        { // no es su reseña
+                          data = {
+                            "nombre": Array(libros)[0].reviews[i].nombre,
+                            "review": Array(libros)[0].reviews[i].review
+                          }
+                          dataSet.push(data);
+                           }
+                      }
+                      if(bandera)
+                      {
+                       data = {
+                          "nombre": req.session.authorization.username,
+                          "review": reviewC
+                        }
+                        dataSet.push(data);
+                        bandera=false
+                        //inserta porque es nuevo
+                      }
+                    }
+                     Array(libros)[0].reviews = dataSet
+                     books[islbC].reviews=Array(libros)[0].reviews
+                     console.log(books[islbC].reviews)
+                     console.log("-----------")
+                     //res.send(`Book with the isbn  ${islbC} updated. new review ${reviewC}`);
+                     return res.send(books[islbC])
+                    }
+      else
+      {
+          res.send("Dont update the review is empty!");
+      }
+      }
+  else{
+      res.send("Unable to find the book!");
+  }
+
+});
+
+// delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const islbC = req.params.isbn;
+    const libros = Object(books[islbC]);
+     if (Object.keys(libros).length>0) 
+     { 
+                    let dataSet=[]
+                    let data
+                    let  bandera =true
+                    if(Array(libros)[0].reviews.length==undefined)
+                    {                    
+                        res.send("Dont delete the list of review is empty!");
+                     }
+                    else
+                    { 
+                      bandera=true
+                      for (let i=0;i<Array(libros)[0].reviews.length;i++)
+                      {
+                         if(req.session.authorization.username==Array(libros)[0].reviews[i].nombre)
+                        {
+                            res.send("delete! "+ Array(libros)[0].reviews[i].review);
+                        }
+                        else
+                        { // no es su reseña
+                          data = {
+                            "nombre": Array(libros)[0].reviews[i].nombre,
+                            "review": Array(libros)[0].reviews[i].review
+                          }
+                          dataSet.push(data);
+                           }
+                      }
+                    }
+                     Array(libros)[0].reviews = dataSet
+                     books[islbC].reviews=Array(libros)[0].reviews
+                     console.log(books[islbC].reviews)
+                     console.log("-----------")
+                    return res.send(books[islbC])
+       }
+  else
+      res.send("Unable to find the book!");
+
 });
 
 module.exports.authenticated = regd_users;
