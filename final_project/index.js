a8b37855ec555a600d0ad5d872ca9806f463341f
j5py <j5py@proton.me>
2024-03-01 00:58:34 +0100

    Format and prepare before adding code

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,27 @@
+
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
+
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
+const port = 5000;
+
 
 app.use(express.json());
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }));
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+
+    // Write the authenication mechanism here
+
 });
- 
-const PORT =5000;
+
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(port, () => console.log("Server is running"));
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,30 +1,43 @@
+
 const express = require('express');
 const jwt = require('jsonwebtoken');
-let books = require("./booksdb.js");
-const regd_users = express.Router();
+const registered = express.Router();
 
+let books = require("./booksdb.js");
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+
+
+const isValid = (username) => { // Returns boolean
+
+    // Write code to check is the username is valid
+
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { // Returns boolean
+
+    // Write code to check if username and password match the one we have in records.
+
 }
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+
+registered.post("/login", (req, res) => { // Only registered users can login
+
+    // Write your code here
+
+    return res.status(300).json({ message: "Yet to be implemented" })
 });
 
-// Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+registered.put("/auth/review/:isbn", (req, res) => { // Add a book review
+
+    // Write your code here
+
+    return res.status(300).json({ message: "Yet to be implemented" })
 });
 
-module.exports.authenticated = regd_users;
+
+
+module.exports.authenticated = registered;
 module.exports.isValid = isValid;
 module.exports.users = users;
