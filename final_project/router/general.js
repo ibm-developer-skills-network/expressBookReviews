4d3f769ea3053f3029ac9820a79ab188be7aa946
j5py <j5py@proton.me>
2024-03-01 06:04:08 +0100

    Get all books based on title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,13 +38,12 @@ public.get('/author/:author', function (req, res) {
     return res.status(404).json({ message: "Not Found" })
 });
 
-
-
-public.get('/title/:title', function (req, res) { // Get all books based on title
-    
-    // Write your code here
-    
-    return res.status(300).json({ message: "Yet to be implemented" })
+public.get('/title/:title', function (req, res) {
+    let result = search(books, "title", req.params.title)
+    if (result) {
+        return res.status(200).json(result)
+    }
+    return res.status(404).json({ message: "Not Found" })
 });
 
 
