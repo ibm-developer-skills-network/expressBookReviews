87135656124dfb67c8e330d976c799da51ef2dfa
helmidh1 <helmidh@hotmail.com>
2024-03-05 14:45:12 -0500

    saving again

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -37,7 +37,7 @@ regd_users.post("/login", (req,res) => {
         req.session.authorization = {
             accessToken, username
         }
-        return res.status(200).send("User successfully logged in");
+        return res.status(200).send("Customer successfully logged in");
     } else {
         return res.status(208).json({ message: "Invalid Login. Check username and password" });
     }
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,9 +24,9 @@ public_users.post("/register", (req,res) => {
     if (username && password) {
         if (!doesExist(username)) { 
         users.push({"username":username,"password":password});
-        return res.status(200).json({message: "User successfully registred. Now you can login"});
+        return res.status(200).json({message: "Customer successfully registred. Now you can login"});
         } else {
-        return res.status(404).json({message: "User already exists!"});    
+        return res.status(404).json({message: "Customer already exists!"});    
         }
     } 
     return res.status(404).json({message: "Unable to register user."});
@@ -66,7 +66,7 @@ public_users.get('/author/:author',function (req, res) {
         let authorBooks = [];
         Object.entries(books).forEach(([key, value]) => {
             if (value.author === author) {
-                authorBooks.push(value.title)
+                authorBooks.push(value)
             }
         })
         resolve(authorBooks)
