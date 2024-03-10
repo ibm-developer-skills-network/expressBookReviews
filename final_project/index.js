420e1bbc215e57bd597f7a2b257240bad0f5dbc0
Nagesh-Vara <nagaesvara@student.tce.edu>
2024-03-10 11:23:08 -0400

    Changed all files

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,28 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req,res,next) {
+    // if the authorization exists there was a log in event
+    if (req.session.authorization) {
+        // get the session token
+        token = req.session.authorization['accessToken'];
+        // verify the token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                // cache the user object in the request
+                req.user = user;
+                next();
+            }
+            // the token has expired or is invalid
+            else {
+                return res.status(403).json({message: "User not authenticated"})
+            }
+        });
+    }
+    // there has not been a log in even in this session
+    else {
+        return res.status(403).json({message: "User not logged in"})
+    }
 });
  
 const PORT =5000;
